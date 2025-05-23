92e0df4d6a2ca92f0d6a386d4a643ffdd4dab40e
Chad Scherrer <chad.scherrer@gmail.com>
2025-05-22 19:02:59 -0700

    requirements

--- src/core/density.rs
+++ src/core/density.rs
@@ -1,5 +1,26 @@
 use super::measure::Measure;
 
+/// A trait representing the log-density between two measures. Goals for the
+/// design:
+/// - for a log-density `l` we should be able to say `l.wrt(new_base_measure)`
+///   and get a new log-density having the same measure but a different base
+///   measure
+/// - `l.at(x)` should represent the log-density of the measure at the point `x`
+///   with respect to the base measure
+/// - `l.at(x)` should be able to be called multiple times with the same `x` and
+///   get the same result
+/// - We should have a way to cache computations when useful, so repeated
+///   computation of `l.at(...)` is as fast as possible
+/// - Computations must be static and stack-allocated when possible
+/// - log-density computations should not be specifically f64 or any other
+///   concrete type. Instead we should be able to say e.g. `let x: f64 =
+///   m.log_density().wrt(b).at(x)` and get an `f64` or any other Float type
+/// - There's an algebra for log-densities:
+///   - If `l` is the log-density of `m` with respect to `b` then `-l` is the
+///     log-density of `b` with respect to `m`
+///   - If `l1` is the log-density of `m1` with respect to `m2` and `l2` is the
+///     log-density of `m2` with respect to `m3` then `l1 + l2` is the
+///     log-density of `m1` with respect to `m3`
 pub trait LogDensityTrait<T> {
     type Measure: Measure<T>;
     type BaseMeasure: Measure<T>;
